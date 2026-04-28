1475ea3fe1aa9cfc08920f388c869815f714c19d
Zachary Uhm <78672384+Re-MoNu@users.noreply.github.com>
2026-04-27 23:52:04 -0500

    Wire main to three-ray hardware and tracker

--- main.py
+++ main.py
@@ -19,13 +19,19 @@ try:
         echo_pin=config.ECHO_PIN,
         min_angle=config.MIN_ANGLE,
         max_angle=config.MAX_ANGLE,
+        i2c_sda=config.I2C_SDA,
+        i2c_scl=config.I2C_SCL,
+        left_xshut=config.LEFT_XSHUT,
+        right_xshut=config.RIGHT_XSHUT,
+        left_addr=config.TOF1_ADDR,
+        right_addr=config.TOF2_ADDR,
     )
 
     print("BOOT: hardware created")
 
     scanner = sonarBins(
         move=scanner_hw.move,
-        read=scanner_hw.read,
+        read=scanner_hw.read_center,
         alert=scanner_hw.alert,
         bins=config.BINS,
         step=config.STEP_ANGLE,
@@ -38,6 +44,7 @@ try:
         servo_base_delay=config.SERVO_BASE_DELAY,
         servo_per_degree_delay=config.SERVO_PER_DEGREE_DELAY,
         post_read_delay=config.POST_READ_DELAY,
+        scan_margin_degrees=config.SCAN_MARGIN_DEGREES,
     )
 
     print("BOOT: sonarBins created")
@@ -45,13 +52,17 @@ try:
     tracker = lidarTracker(
         bins_engine=scanner,
         move=scanner_hw.move,
-        read=scanner_hw.read,
+        read_center=scanner_hw.read_center,
+        read_left=scanner_hw.read_left,
+        read_right=scanner_hw.read_right,
         alert=scanner_hw.alert,
         track_step=config.TRACK_STEP,
         lost_limit=config.TRACK_LOST_LIMIT,
         settle_delay=config.TRACK_SETTLE_DELAY,
         min_score=config.TRACK_MIN_SCORE,
         max_cycles=config.TRACK_MAX_CYCLES,
+        left_offset=config.LEFT_SENSOR_OFFSET,
+        right_offset=config.RIGHT_SENSOR_OFFSET,
         debug=config.DEBUG,
     )
 
@@ -72,11 +83,7 @@ try:
 
         if result is not False:
             print("Deviation detected:", result)
-
-            if isinstance(result, (int, float)):
-                tracker.track(result)
-            else:
-                print("TRACK SKIPPED: sweep did not return angle")
+            tracker.track(result)
 
 except Exception as e:
     print("CRASH:", type(e).__name__)
